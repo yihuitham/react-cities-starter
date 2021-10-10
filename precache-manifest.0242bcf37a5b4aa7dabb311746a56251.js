d1808c1c6f860fe7b0febcdce200a28305f8b114
YihuiTham <yihuitham@gmail.com>
2021-10-10 12:53:01 +0800

    Updates

---
+++ precache-manifest.0242bcf37a5b4aa7dabb311746a56251.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2362950fd36d96a440c79baf7f5e370",
+    "url": "/react-cities-starter/index.html"
+  },
+  {
+    "revision": "371fbb547cdba66cc10a",
+    "url": "/react-cities-starter/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "3c2968ef8b2eb466d66b",
+    "url": "/react-cities-starter/static/js/2.32bdae52.chunk.js"
+  },
+  {
+    "revision": "371fbb547cdba66cc10a",
+    "url": "/react-cities-starter/static/js/main.da8c351f.chunk.js"
+  },
+  {
+    "revision": "c73938739d0f5e53399e",
+    "url": "/react-cities-starter/static/js/runtime~main.b95ab25e.js"
+  }
+]);
